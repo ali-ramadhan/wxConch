25b02fc64cb59de9a41fd4b08b95934dbbe5cbc7
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-11 17:16:53 -0400

    Modular Dark Sky T time series grabber

--- dark_sky.py
+++ dark_sky.py
@@ -1,65 +1,71 @@
 from datetime import datetime
 import requests
+import logging
 import json
 
-import matplotlib.pyplot as plt
-
-# City data
-lat, lon = 42.362389, -71.091083
+logging.basicConfig(format="[%(asctime)s.%(msecs)03d] %(funcName)s:%(levelname)s: %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
 
 # Dark Sky API
-secret_key = "81c07948c439765d630c4c60fc56634e"
-ds_forecast_url = "https://api.darksky.net/forecast/" + secret_key + "/" + str(lat) + "," + str(lon)
+DARK_SKY_KEY = "81c07948c439765d630c4c60fc56634e"
 
-ds_response = requests.get(ds_forecast_url)
-ds_forecast = json.loads(ds_response.content)
 
-city_lat = ds_forecast['latitude']
-city_lon = ds_forecast['longitude']
-city_tz = ds_forecast['timezone']
+def dark_sky_temp_time_series(lat, lon):
+    forecast_url = "https://api.darksky.net/forecast/" + DARK_SKY_KEY + "/" + str(lat) + "," + str(lon)
 
-ds_currently = ds_forecast['currently']
-current_time = ds_currently['time']
-summary = ds_currently['summary']
-nearest_storm_distance = ds_currently['nearestStormDistance']
-precip_intensity = ds_currently['precipIntensity']
-precip_prob = ds_currently['precipProbability']
-T = ds_currently['temperature']
-T_apparent = ds_currently['apparentTemperature']
-dew_point = ds_currently['dewPoint']
-humidity = ds_currently['humidity']
-pressure = ds_currently['pressure']
-wind_speed = ds_currently['windSpeed']
-wind_gust = ds_currently['windGust']
-wind_bearing = ds_currently['windBearing']
-cloud_cover = ds_currently['cloudCover']
-uv_index = ds_currently['uvIndex']
-visibility = ds_currently['visibility']
+    response = requests.get(forecast_url)
+    
+    if response.status_code != 200:
+        response.raise_for_status()
+    
+    forecast = json.loads(response.content)
 
-ts = []
-Ts = []
+    city_lat = forecast['latitude']
+    city_lon = forecast['longitude']
+    city_tz = forecast['timezone']
 
-ds_hourly_forecast = ds_forecast['hourly']['data']
-for i, forecast in enumerate(ds_hourly_forecast):
-    time = datetime.utcfromtimestamp(forecast['time']).strftime('%Y-%m-%d %H:%M:%S')
-    summary = forecast['summary']
-    precip_intensity = forecast['precipIntensity']
-    precip_prob = forecast['precipProbability']
-    T = forecast['temperature']
-    T_apparent = forecast['apparentTemperature']
-    dew_point = forecast['dewPoint']
-    humidity = forecast['humidity']
-    pressure = forecast['pressure']
-    wind_speed = forecast['windSpeed']
-    wind_gust = forecast['windGust']
-    wind_bearing = forecast['windBearing']
-    cloud_cover = forecast['cloudCover']
-    uv_index = forecast['uvIndex']
-    visibility = forecast['visibility']
-    print("{:s} T={:.1f}°F".format(time, T))
+    logging.info("Dark Sky: (lat, lon) = ({:.2f}, {:.2f}), timezone={:}".format(city_lat, city_lon, city_tz))
 
-    ts.append(time)
-    Ts.append(T)
+    currently = forecast['currently']
+    current_time = currently['time']
+    summary = currently['summary']
+    nearest_storm_distance = currently['nearestStormDistance']
+    precip_intensity = currently['precipIntensity']
+    precip_prob = currently['precipProbability']
+    T = currently['temperature']
+    T_apparent = currently['apparentTemperature']
+    dew_point = currently['dewPoint']
+    humidity = currently['humidity']
+    pressure = currently['pressure']
+    wind_speed = currently['windSpeed']
+    wind_gust = currently['windGust']
+    wind_bearing = currently['windBearing']
+    cloud_cover = currently['cloudCover']
+    uv_index = currently['uvIndex']
+    visibility = currently['visibility']
 
-plt.plot(ts, Ts)
-plt.show()
+    times = []
+    temps = []
+
+    hourly_forecast = forecast['hourly']['data']
+    for i, forecast in enumerate(hourly_forecast):
+        time = datetime.utcfromtimestamp(forecast['time'])
+        summary = forecast['summary']
+        precip_intensity = forecast['precipIntensity']
+        precip_prob = forecast['precipProbability']
+        T = forecast['temperature']
+        T_apparent = forecast['apparentTemperature']
+        dew_point = forecast['dewPoint']
+        humidity = forecast['humidity']
+        pressure = forecast['pressure']
+        wind_speed = forecast['windSpeed']
+        wind_gust = forecast['windGust']
+        wind_bearing = forecast['windBearing']
+        cloud_cover = forecast['cloudCover']
+        uv_index = forecast['uvIndex']
+        visibility = forecast['visibility']
+    
+        times.append(time)
+        temps.append(T)
+
+    return times, temps

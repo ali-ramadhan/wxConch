bb53a5c9baea00bc1f305cb7d99fce73b2155ab4
Ali Ramadhan <ali.hh.ramadhan@gmail.com>
2019-06-12 06:53:54 -0400

    Util function to download file with tqdm progress bar

--- utils.py
+++ utils.py
@@ -1,9 +1,24 @@
+import os
+import time
+import math
+import logging
+import requests
+from datetime import datetime
+
 import smtplib, ssl
 from os.path import basename
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
+
+from tqdm import tqdm
+
+
+HEADERS = {
+    "User-Agent": "wxConch (Python3.7) https://github.com/ali-ramadhan/wxConch",
+    "From": "alir@mit.edu"
+}
 
 
 def send_email(send_from, send_to, subject, text, files=None):
@@ -34,3 +49,49 @@ def send_email(send_from, send_to, subject, text, files=None):
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(send_from, password)
         server.sendmail(send_from, send_to, msg.as_string())
+
+
+def download_file(url, local_filepath, max_retries=1, retry_timeout=60):
+    # Removing illegal characters
+    # local_filepath = local_filepath.replace('?', '')
+    # local_filepath = local_filepath.replace('\"', '')
+    #
+    # basename = os.path.basename(local_filepath)
+    # basename = basename.replace(':', '')
+    # local_filepath = os.path.join(os.path.dirname(local_filepath), basename)
+
+    chunk_size = 1024  # [bytes]
+    wrote = 0  # [bytes]
+
+    response = requests.get(url, stream=True, headers=HEADERS)
+
+    if response.status_code != 200:
+        response.raise_for_status()
+
+    file_size = int(response.headers.get("content-length"))  # [bytes]
+    tqdm_total = math.ceil(file_size // chunk_size)
+
+    if os.path.isfile(local_filepath):
+        if os.path.getsize(local_filepath) == file_size:
+            logging.info("{:s} ({:,d} bytes) already downloaded. Skipping.".format(local_filepath, file_size))
+            return
+
+    while True:
+        try:
+            with open(local_filepath, 'wb') as f:
+                for chunk in tqdm(response.iter_content(chunk_size=chunk_size),
+                                  desc=local_filepath, leave=True, total=tqdm_total,
+                                  unit='KiB', unit_divisor=1024, unit_scale=True):
+                    wrote = wrote + len(chunk)
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+            break
+        except Exception as ex:
+            logging.error(ex)
+            logging.info("[{:}] Sleeping for {:s} seconds before retry...\n".format(str(datetime.now()), retry_timeout))
+            time.sleep(retry_timeout)
+
+    if file_size != 0 and wrote != file_size:
+        logging.error("Total file size does not match bytes written!")
+
+    return
